ffb97ff680b6e9e82af28ab6f621f23e51522993
Christin Carter <raspberry.muppet@gmail.com>
2023-01-23 16:28:25 -0700

    added more info to schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -4,17 +4,26 @@ CREATE DATABASE employees_db;
 USE employees_db;
 
 CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+    department_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
     department_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    role_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL, 
     salary DECIMAL NOT NULL, 
-    department_id INT NOT NULL
+    dept_id INT NOT NULL, 
+    FOREIGN KEY (dept_id) REFERENCES department(department_id)
 ); 
 
 CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
-)
+    employee_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+    first_name VARCHAR(30) NOT NULL, 
+    last_name VARCHAR(30) NOT NULL, 
+    job_title VARCHAR (30) NOT NULL, 
+    FOREIGN KEY (job_title) REFERENCES role(title), 
+    salary DECIMAL NOT NULL, 
+    FOREIGN KEY (salary) REFERENCES role(salary), 
+    -- manager_id INT NOT NULL, 
+    -- FOREIGN KEY ()
+);
